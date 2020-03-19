54999820202911a9d1242b1fa538fd9feee6d260
Marcelo Chiaradia <mchiaradia@suse.com>
2020-03-19 16:04:40 +0100

    CodecRequest tests

--- server/server_codec_test.go
+++ server/server_codec_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -152,15 +153,15 @@ func Test_NewRequest(t *testing.T) {
 		expectedCodecRequest CodecRequest
 	}{
 		{name: "Create new CodecRequest Success",
-			httpRequest:          buildSuccessHTTPRequest(),
-			expectedCodecRequest: buildSuccessCodecRequest(),
+			httpRequest:          buildSuccessHTTPRequestWithBody(successRequestBody),
+			expectedCodecRequest: buildSuccessCodecRequestWithBody(successRequestBody),
 		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			codec := NewCodec()
-			codec.RegisterDefaultParser(new(StructParser))
+			codec.RegisterDefaultParser(new(MulticastArgsParser))
 			codecRequest := codec.NewRequest(tc.httpRequest)
 
 			if !reflect.DeepEqual(codecRequest, &tc.expectedCodecRequest) {
@@ -170,30 +171,171 @@ func Test_NewRequest(t *testing.T) {
 	}
 }
 
-func buildSuccessCodecRequest() CodecRequest {
+const sessionKey = "39x0d8f9d78559bf45bc41d5145444fc4e05489549f2f99a8d30768b478fe219dc2"
+
+var serverIds = []int64{1000010001, 1000010002}
+
+var serverArgs = [][]interface{}{{"arg1_Server1", "arg1_Server2"}, {"arg2_Server1", "arg2_Server2"}}
+
+func Test_CodecRequest_ReadRequest(t *testing.T) {
+	tt := []struct {
+		name                 string
+		httpRequest          *http.Request
+		expectedCodecRequest CodecRequest
+		structToHydrate      interface{}
+		expectedStruct       MulticastArgs
+		expectedError        bool
+	}{
+		{name: "Create new CodecRequest Success",
+			httpRequest:          buildSuccessHTTPRequestWithBody(successRequestBodyForMulticastArgs),
+			expectedCodecRequest: buildSuccessCodecRequestWithBody(successRequestBodyForMulticastArgs),
+			structToHydrate:      &MulticastArgs{},
+			expectedStruct:       MulticastArgs{HubSessionKey: sessionKey, ServerIDs: serverIds, ServerArgs: serverArgs},
+			expectedError:        false,
+		},
+		{name: "Create new CodecRequest no_pointer_structToHydrate_pass Failed",
+			httpRequest:          buildSuccessHTTPRequestWithBody(successRequestBodyForMulticastArgs),
+			expectedCodecRequest: buildSuccessCodecRequestWithBody(successRequestBodyForMulticastArgs),
+			structToHydrate:      MulticastArgs{},
+			expectedStruct:       MulticastArgs{},
+			expectedError:        true,
+		},
+		{name: "Create new CodecRequest error_when_unmarshalling Failed",
+			httpRequest:          buildSuccessHTTPRequestWithBody(brokenRequestBodyForMulticastArgs),
+			expectedCodecRequest: buildSuccessCodecRequestWithBody(brokenRequestBodyForMulticastArgs),
+			structToHydrate:      &MulticastArgs{},
+			expectedStruct:       MulticastArgs{},
+			expectedError:        true,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			codec := NewCodec()
+			codec.RegisterDefaultParser(new(MulticastArgsParser))
+
+			codecRequest := codec.NewRequest(tc.httpRequest)
+
+			if !reflect.DeepEqual(codecRequest, &tc.expectedCodecRequest) {
+				t.Fatalf("Expected and actual don't match")
+			}
+
+			err := codecRequest.ReadRequest(tc.structToHydrate)
+
+			if err != nil && !tc.expectedError {
+				t.Fatalf("Error ocurred when reading request. %v", err)
+			}
+
+			if err == nil && !reflect.DeepEqual(tc.structToHydrate, &tc.expectedStruct) {
+				t.Fatalf("Expected and actual don't match")
+			}
+		})
+	}
+}
+
+func buildSuccessCodecRequestWithBody(body string) CodecRequest {
 	var request ServerRequest
-	rawxml := []byte(successRequestBody)
+	rawxml := []byte(body)
 	xml.Unmarshal(rawxml, &request)
 	request.rawxml = rawxml
 
-	return CodecRequest{request: &request, parser: new(StructParser)}
+	return CodecRequest{request: &request, parser: new(MulticastArgsParser)}
 }
 
-func buildSuccessHTTPRequest() *http.Request {
-	httpRequest := httptest.NewRequest(http.MethodPost, "http://localhost:8888/hub/rpc/api", strings.NewReader(successRequestBody))
+func buildSuccessHTTPRequestWithBody(body string) *http.Request {
+	httpRequest := httptest.NewRequest(http.MethodPost, "http://localhost:8888/hub/rpc/api", strings.NewReader(body))
 	httpRequest.Header.Set("Content-Type", "text/xml")
 
 	return httpRequest
 }
 
 const successRequestBody = `
-			<methodName>hub.login</methodName>
-				<params>
-					<param>
-						<value><string>admin</string></value>
-					</param>
-					<param>
-						<value><string>admin</string></value>
-					</param>
-				</params>
-			</methodCall>`
+<?xml version='1.0'?>
+<methodCall>
+	<methodName>multicast.system.listUserSystems</methodName>
+	<params>
+		<param>
+			<value><string>39x0d8f9d78559bf45bc41d5145444fc4e05489549f2f99a8d30768b478fe219dc2</string></value>
+		</param><methodCall>
+		<methodName>hub.login</methodName>
+		<params>
+			<param>
+				<value><string>admin</string></value>
+			</param>
+			<param>
+				<value><string>admin</string></value>
+			</param>
+		</params>
+	</methodCall>
+		<param>
+			<value><array><data>
+			<value><int>1000010000</int></value>
+			<value><int>1000010001</int></value>
+			</data></array></value>
+		</param>
+		<param>
+			<value><array><data>
+			<value><string>admin</string></value>
+			<value><string>admin</string></value>
+			</data></array></value>
+		</param>
+	</params>
+</methodCall>`
+
+var successRequestBodyForMulticastArgs = `
+<?xml version='1.0'?>
+<methodCall>
+	<methodName>multicast.system.listUserSystems</methodName>
+	<params>
+		<param>
+			<value><string>` + sessionKey + `</string></value>
+		</param>
+		<param>
+			<value><array><data>
+			<value><int>` + strconv.FormatInt(serverIds[0], 10) + `</int></value>
+			<value><int>` + strconv.FormatInt(serverIds[1], 10) + `</int></value>
+			</data></array></value>
+		</param>
+		<param>
+			<value><array><data>
+			<value><string>` + serverArgs[0][0].(string) + `</string></value>
+			<value><string>` + serverArgs[0][1].(string) + `</string></value>
+			</data></array></value>
+		</param>
+		<param>
+			<value><array><data>
+			<value><string>` + serverArgs[1][0].(string) + `</string></value>
+			<value><string>` + serverArgs[1][1].(string) + `</string></value>
+			</data></array></value>
+		</param>
+	</params>
+</methodCall>`
+
+var brokenRequestBodyForMulticastArgs = `
+<?xml version='1.0'?>
+<methodCall>
+	<methodName>multicast.system.listUserSystems</methodName>
+	<params>
+		<param>
+			<value><string>` + sessionKey + `</string></value>
+		</param>
+		<param>
+			<value><array><data>
+			<value><int>abd</int></value>
+			<value><int>` + strconv.FormatInt(serverIds[1], 10) + `</int></value>
+			</data></array></value>
+		</param>
+		<param>
+			<value><array><data>
+			<value><string>` + serverArgs[0][0].(string) + `</string></value>
+			<value><string>` + serverArgs[0][1].(string) + `</string></value>
+			</data></array></value>
+		</param>
+		<param>
+			<value><array><data>
+			<value><string>` + serverArgs[1][0].(string) + `</string></value>
+			<value><string>` + serverArgs[1][1].(string) + `</string></value>
+			</data></array></value>
+		</param>
+	</params>
+</methodCall>`
