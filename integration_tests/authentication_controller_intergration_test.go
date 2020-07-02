9fc33a6ab4426c5d51007227d05a1e973f191eee
Abid Mehmood <amehmood@suse.de>
2020-07-02 13:11:44 +0200

    intgeration tests for auth modes

--- integration_tests/authentication_controller_intergration_test.go
+++ integration_tests/authentication_controller_intergration_test.go
@@ -1,239 +1,198 @@
 package integration_tests
 
-// import (
-// 	"os"
-// 	"regexp"
-// 	"strings"
-// 	"sync"
-// 	"testing"
+import (
+	"strings"
+	"testing"
 
-// 	"github.com/uyuni-project/hub-xmlrpc-api/client"
-// 	"github.com/uyuni-project/hub-xmlrpc-api/config"
-// 	"github.com/uyuni-project/hub-xmlrpc-api/controller"
-// 	"github.com/uyuni-project/hub-xmlrpc-api/controller/transformer"
-// 	"github.com/uyuni-project/hub-xmlrpc-api/gateway"
-// 	"github.com/uyuni-project/hub-xmlrpc-api/session"
-// 	"github.com/uyuni-project/hub-xmlrpc-api/uyuni"
-// )
+	"github.com/uyuni-project/hub-xmlrpc-api/client"
+)
 
-// func init() {
-// 	// load test data
-// 	os.Setenv("HUB_CONFIG_FILE", "../tests/config.json")
-// }
+func TestLoginManual(t *testing.T) {
 
-// func TestLogin(t *testing.T) {
-// 	tt := []struct {
-// 		name     string
-// 		username string
-// 		password string
-// 		err      string
-// 	}{
-// 		{name: "Invalid credentials", username: "unknown-user", password: "unknown-user", err: controller.FaultInvalidCredentials.Message},
-// 		{name: "Valid credentials", username: "admin", password: "admin"},
-// 	}
+	//config.InitializeConfig()
+	const errorMessage = "Either the password or username is incorrect"
+	tt := []struct {
+		name          string
+		username      string
+		password      string
+		expectedError string
+	}{
+		{name: "Valid credentials", username: "admin", password: "admin"},
+		{name: "Invalid credentials", username: "unknown", password: "unknown", expectedError: errorMessage},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
 
-// 	for _, tc := range tt {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			//setup env
-// 			conf := config.NewConfig()
-// 			client := client.NewClient(conf.ConnectTimeout, conf.RequestTimeout)
+			client := client.NewClient(10, 10)
 
-// 			var syncMap sync.Map
-// 			hubSessionRepository := session.NewInMemoryHubSessionRepository(&syncMap)
-// 			serverSessionRepository := session.NewInMemoryServerSessionRepository(&syncMap)
+			//login
+			credentials := []interface{}{tc.username, tc.password}
+			loginResponse, err := client.ExecuteCall(gatewayServerURL, "hub.login", credentials)
 
-// 			uyuniCallExecutor := uyuni.NewUyuniCallExecutor(client)
-// 			uyuniAuthenticator := uyuni.NewUyuniAuthenticator(uyuniCallExecutor)
-// 			uyuniTopologyInfoRetriever := uyuni.NewUyuniTopologyInfoRetriever(uyuniCallExecutor)
+			// if it's no error then defer the call for closing body
+			if err != nil {
+				if !strings.Contains(err.Error(), tc.expectedError) {
+					t.Fatalf("Unexpected Error message: `%v` doesn't contain `%v`", err, tc.expectedError)
+					return
+				}
+			} else {
+				hubSessionKey := loginResponse.(string)
+				if len(strings.TrimSpace(hubSessionKey)) == 0 {
+					t.Fatalf("Unexpected Result: empty session key was returned.")
+				}
 
-// 			serverAuthenticator := gateway.NewServerAuthenticator(conf.HubAPIURL, uyuniAuthenticator, uyuniTopologyInfoRetriever, hubSessionRepository, serverSessionRepository)
-// 			hubLoginer := gateway.NewHubLoginer(conf.HubAPIURL, uyuniAuthenticator, serverAuthenticator, uyuniTopologyInfoRetriever, hubSessionRepository)
+			}
+		})
+	}
 
-// 			loginController := controller.NewHubLoginController(hubLoginer, transformer.MulticastResponseTransformer)
+}
 
-// 			reply := struct{ Data string }{""}
-// 			err := loginController.Login(nil, &controller.LoginRequest{tc.username, tc.password}, &reply)
+func TestLoginAutoConnect(t *testing.T) {
 
-// 			if err != nil && !strings.Contains(err.Error(), tc.err) {
-// 				t.Fatalf("Expected %v, Got %v", tc.err, err.Error())
-// 			}
+	//config.InitializeConfig()
+	const errorMessage = "Either the password or username is incorrect"
+	tt := []struct {
+		name          string
+		username      string
+		password      string
+		expectedError string
+	}{
+		{name: "Valid credentials", username: "admin", password: "admin"},
+		{name: "Invalid credentials", username: "unknown", password: "unknown", expectedError: errorMessage},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			client := client.NewClient(10, 10)
+			//login
+			credentials := []interface{}{tc.username, tc.password}
+			loginResponse, err := client.ExecuteCall(gatewayServerURL, "hub.loginWithAutoconnectMode", credentials)
+			if err != nil {
+				if !strings.Contains(err.Error(), tc.expectedError) {
+					t.Fatalf("Unexpected Error message: `%v` doesn't contain `%v`", err, tc.expectedError)
+					return
+				}
+			} else {
+				loginResponseMap := loginResponse.(map[string]interface{})
+				hubSessionKey := loginResponseMap["SessionKey"].(string)
+				if len(strings.TrimSpace(hubSessionKey)) == 0 {
+					t.Fatalf("Unexpected Result: empty session key was returned.")
+				}
+				validatePrephiralServersResponse(t, loginResponse)
+			}
+		})
+	}
 
-// 			// test the hubkey
-// 			hubsessionkey := reply.Data
+}
+func TestLoginRelayMode(t *testing.T) {
 
-// 			matched, _ := regexp.MatchString(`^[A-Za-z0-9]{68}$`, hubsessionkey)
-// 			if !matched {
-// 				t.Fatalf("Unexepected token pattern %v", hubsessionkey)
-// 				return
-// 			}
+	const errorMessage = "Either the password or username is incorrect"
+	tt := []struct {
+		name          string
+		username      string
+		password      string
+		expectedError string
+	}{
+		{name: "Valid credentials", username: "admin", password: "admin"},
+		{name: "Invalid credentials", username: "unknown", password: "unknown", expectedError: errorMessage},
+	}
 
-// 			hubSession := hubSessionRepository.RetrieveHubSession(hubsessionkey)
-// 			if hubSession.HubSessionKey != hubsessionkey {
-// 				t.Fatalf("User name doesn't match with the key, expected %v, got %v", hubSession.HubSessionKey, hubsessionkey)
-// 			}
-// 		})
-// 	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			client := client.NewClient(10, 10)
 
-// }
+			//login
+			credentials := []interface{}{tc.username, tc.password}
+			loginResponse, err := client.ExecuteCall(gatewayServerURL, "hub.loginWithAuthRelayMode", credentials)
 
-// func TestLoginAutoconnect(t *testing.T) {
-// 	tt := []struct {
-// 		name     string
-// 		username string
-// 		password string
-// 		err      string
-// 	}{
-// 		{name: "Valid credentials", username: "admin", password: "admin"},
-// 		{name: "Invalid credentials", username: "unknown-user", password: "unknown-user", err: controller.FaultInvalidCredentials.Message},
-// 	}
+			if err != nil {
+				if !strings.Contains(err.Error(), tc.expectedError) {
+					t.Fatalf("Unexpected Error message: `%v` doesn't contain `%v`", err, tc.expectedError)
+					return
+				}
+			} else {
+				hubSessionKey := loginResponse.(string)
+				if len(strings.TrimSpace(hubSessionKey)) == 0 {
+					t.Fatalf("Unexpected Result: empty session key was returned.")
+				}
 
-// 	for _, tc := range tt {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			//setup env
-// 			conf := config.NewConfig()
-// 			client := client.NewClient(conf.ConnectTimeout, conf.ReadWriteTimeout)
+			}
+		})
+	}
 
-// 			var syncMap sync.Map
-// 			hubSessionRepository := session.NewInMemoryHubSessionRepository(&syncMap)
-// 			serverSessionRepository := session.NewInMemoryServerSessionRepository(&syncMap)
+}
 
-// 			uyuniCallExecutor := uyuni.NewUyuniCallExecutor(client)
-// 			uyuniAuthenticator := uyuni.NewUyuniAuthenticator(uyuniCallExecutor)
-// 			uyuniTopologyInfoRetriever := uyuni.NewUyuniTopologyInfoRetriever(uyuniCallExecutor)
+func TestAttachToServers(t *testing.T) {
+	const errorMessage = "Either the password or username is incorrect"
+	tt := []struct {
+		name          string
+		username      string
+		password      string
+		expectedError string
+	}{
+		{name: "Valid credentials", username: "admin", password: "admin"},
+		{name: "Invalid credentials", username: "unknown", password: "unknown", expectedError: errorMessage},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
 
-// 			serverAuthenticator := gateway.NewServerAuthenticator(conf.Hub.SUMA_API_URL, uyuniAuthenticator, uyuniTopologyInfoRetriever, hubSessionRepository, serverSessionRepository)
-// 			hubLoginer := gateway.NewHubLoginer(conf.Hub.SUMA_API_URL, uyuniAuthenticator, serverAuthenticator, uyuniTopologyInfoRetriever, hubSessionRepository)
+			client := client.NewClient(10, 10)
 
-// 			loginController := controller.NewHubLoginController(hubLoginer, transformer.MulticastResponseTransformer)
+			//login
+			credentials := []interface{}{tc.username, tc.password}
+			loginResponse, err := client.ExecuteCall(gatewayServerURL, "hub.loginWithAuthRelayMode", credentials)
 
-// 			reply := struct {
-// 				Data *controller.LoginWithAutoconnectModeResponse
-// 			}{}
-// 			err := loginController.LoginWithAutoconnectMode(nil, &controller.LoginRequest{tc.username, tc.password}, &reply)
-// 			if err != nil && !strings.Contains(err.Error(), tc.err) {
-// 				t.Fatalf("Expected %v, Got %v", tc.err, err.Error())
-// 			}
+			// if it's no error then defer the call for closing body
+			if err != nil {
+				if !strings.Contains(err.Error(), tc.expectedError) {
+					t.Fatalf("Unexpected Error message: `%v` doesn't contain `%v`", err, tc.expectedError)
+					return
+				}
+			} else {
+				hubSessionKey := loginResponse.(string)
+				if len(strings.TrimSpace(hubSessionKey)) == 0 {
+					t.Fatalf("Unexpected Result: empty session key was returned.")
+				}
+				loggedInServerIDs := make([]int64, 0, len(peripheralServers))
+				for _, serverID := range peripheralServers {
+					loggedInServerIDs = append(loggedInServerIDs, serverID.id)
+				}
+				//Call attachToServers method
+				attachToServerResponse, err := client.ExecuteCall(gatewayServerURL, "hub.attachToServers", []interface{}{hubSessionKey, loggedInServerIDs})
+				if err != nil {
+					t.Fatalf("Unexpected Error message: `%v`", err)
+					return
 
-// 			// test the hubkey
-// 			hubSessionKey := reply.Data.SessionKey
+				}
+				validatePrephiralServersResponse(t, attachToServerResponse)
+			}
+		})
+	}
 
-// 			matched, _ := regexp.MatchString(`^[A-Za-z0-9]{68}$`, hubSessionKey)
-// 			if !matched {
-// 				t.Fatalf("Unexepected token pattern %v", hubSessionKey)
-// 				return
-// 			}
+}
 
-// 			hubSession := hubSessionRepository.RetrieveHubSession(hubSessionKey)
-// 			if hubSession.HubSessionKey != hubSessionKey {
-// 				t.Fatalf("User name doesn't match with the key, expected %v, got %v", hubSession.HubSessionKey, hubSessionKey)
-// 			}
+func validatePrephiralServersResponse(t *testing.T, authResponse interface{}) {
+	loginResponseMap := authResponse.(map[string]interface{})
+	succeededServers := loginResponseMap["Successful"].(map[string]interface{})
+	failedServers := loginResponseMap["Failed"].(map[string]interface{})
 
-// 			//test if servers attached to hub have also been authenticated automatically
-// 			serverIDs := struct{ Data []int64 }{}
-// 			err = uyuniTopologyInfoRetriever.ListServerIDs(hubSessionKey), &serverIDs)
+	if len(succeededServers) != len(peripheralServers) {
+		t.Fatalf("Unexpected Result: Some servers failed unexpectedly")
+	}
+	if len(failedServers["ServerIds"].([]interface{})) != 0 {
+		t.Fatalf("Unexpected Result: there should not be failed severs")
+	}
 
-// 			for _, serverID := range serverIDs.Data {
-// 				serverSession := serverSessionRepository.RetrieveServerSessionByServerID(hubSessionKey, serverID)
-// 				if len(serverSession.url) == 0 {
-// 					t.Fatalf("Expected valid url for server with severId: %v, got empty instead %v", serverID, serverSession.url)
-// 				}
-// 				if len(serverSession.sessionkey) <= 0 {
-// 					t.Fatalf("Expected valid SessionKey for server with severId: %v, Got %v", serverID, serverSession.sessionkey)
-// 				}
-// 			}
-// 		})
-// 	}
-// }
+	serverIDs := succeededServers["ServerIds"].([]interface{})
+	serverSessionKeys := succeededServers["Responses"].([]interface{})
+	for _, sid := range serverIDs {
+		if sid.(int64) == 0 {
+			t.Fatalf("Unexpected Result: empty session key was returned.")
+		}
+	}
+	for _, skey := range serverSessionKeys {
+		if len(strings.TrimSpace(skey.(string))) == 0 {
+			t.Fatalf("Unexpected Result: session key was empty.")
+		}
 
-// func TestAttachToServers(t *testing.T) {
-// 	tt := []struct {
-// 		name     string
-// 		username string
-// 		password string
-// 		err      string
-// 	}{
-// 		{name: "Valid credentials", username: "admin", password: "admin"},
-// 	}
-
-// 	for _, tc := range tt {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			conf := config.InitializeConfig()
-// 			client := client.NewClient(conf.ConnectTimeout, conf.ReadWriteTimeout)
-// 			session := session.NewSession()
-// 			hub := server.NewHubService(client, session, conf.Hub.SUMA_API_URL)
-
-// 			//login
-// 			reply := struct{ Data string }{""}
-// 			err = hub.LoginWithAuthRelayMode(nil, &server.LoginArgs{tc.username, tc.password}, &reply)
-// 			if err != nil {
-// 				t.Fatalf("Login faied with error : %v", err)
-// 			}
-// 			sessionKey := struct{ HubSessionKey string }{reply.Data}
-
-// 			// List server Ids
-// 			serverIDsReply := struct{ Data []int64 }{}
-// 			err = hub.ListServerIds(req, &sessionKey, &serverIDsReply)
-// 			serverIDs := serverIDsReply.Data
-
-// 			srvArgs := server.MulticastArgs{"method", sessionKey.HubSessionKey, serverIDs, nil}
-// 			err = hub.AttachToServers(req, &srvArgs, &struct{ Data []error }{})
-// 			if err != nil && err.Error() != tc.err {
-// 				t.Fatalf("Unexpected Result: Exepected %v, Got %v", tc.err, err.Error())
-// 				return
-// 			}
-// 			for _, s := range serverIDs {
-// 				serverSession := session.RetrieveServerSessionByServerID(sessionKey.HubSessionKey, s)
-// 				if len(serverSession.url) == 0 {
-// 					t.Fatalf("Expected valid url for server with severId: %v, got empty instead %v", s, serverSession.url)
-// 				}
-// 				if len(serverSession.sessionKey) <= 0 {
-// 					t.Fatalf("Expected valid SessionKey for server with severId: %v, Got %v", s, serverSession.sessionKey)
-// 				}
-// 			}
-// 		})
-// 	}
-// }
-
-// func TestListServerIds(t *testing.T) {
-// 	tt := []struct {
-// 		name     string
-// 		username string
-// 		password string
-// 		err      string
-// 	}{
-// 		{name: "Valid credentials", username: "admin", password: "admin"},
-// 		{name: "With invalid  credentials", username: "unknownadmin", password: "unknownadmin", err: codec.FaultInvalidCredentials.Message},
-// 	}
-
-// 	for _, tc := range tt {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			conf := config.InitializeConfig()
-// 			client := client.NewClient(conf.ConnectTimeout, conf.ReadWriteTimeout)
-// 			session := session.NewSession()
-// 			hub := server.NewHubService(client, session, conf.Hub.SUMA_API_URL)
-
-// 			req, err := http.NewRequest("GET", conf.Hub.SUMA_API_URL, nil)
-// 			if err != nil {
-// 				t.Fatalf("could not create request: %v", err)
-// 			}
-// 			reply := struct{ Data string }{""}
-// 			err = hub.Login(req, &server.LoginArgs{tc.username, tc.password}, &reply)
-// 			if err != nil {
-// 				if !strings.Contains(err.Error(), tc.err) {
-// 					t.Fatalf("Expected %v, Got %v", tc.err, err.Error())
-// 				}
-// 				return
-// 			}
-// 			serverIdsreply := struct{ Data []int64 }{}
-// 			err = hub.ListServerIds(req, &struct{ HubSessionKey string }{reply.Data}, &serverIdsreply)
-// 			if err != nil && err.Error() != tc.err {
-// 				t.Fatalf("Unexpected Result: Exepected %v, Got %v", tc.err, err.Error())
-// 				return
-// 			}
-// 			serverIds := len(serverIdsreply.Data)
-// 			if serverIds <= 0 {
-// 				t.Fatalf("Unexpected Result: Expected some servers, got nothing")
-// 			}
-// 		})
-// 	}
-// }
+	}
+}
